fd5f7f4a5b9115baf77b5f94e13a225b65f10679
LucaLogallo <75795162+LucaLogallo@users.noreply.github.com>
2022-04-06 17:07:20 +0200

    Add files via upload

---
+++ custom_components/freehands/const.py
@@ -0,0 +1,10 @@
+"""Constants for the freehandsmiddleware integration."""
+
+DOMAIN = "freehands"
+
+BROKER = "192.168.3.122"
+PORT = "1883"
+TOPIC = "zigbee2mqtt/+"
+USERNAME = "mqtt_user"
+PASSWORD = "%%7!P6C6zji@VADv"
+URI = "mqtt://" + USERNAME + ":" + PASSWORD + "@" + BROKER + "." + PORT
